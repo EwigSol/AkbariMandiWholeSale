7e6e52dd0faec9e647072e256ad74ee03455fdf1
EwigLife <zain@ewiglife.com>
2022-06-13 22:21:07 +0500

    Getx added

---
+++ akbarimandiwholesale/lib/views/Home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
